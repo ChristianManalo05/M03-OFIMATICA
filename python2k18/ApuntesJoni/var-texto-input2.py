0a3863ee4eefa30a3e6463d0a18bf43a086e0df9
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-06 11:57:03 +0100

    Create var-texto-input2.py

---
+++ python2k18/ApuntesJoni/var-texto-input2.py
@@ -0,0 +1,8 @@
+nombre = raw_input("Indique su nombre: ")
+apellido1 = raw_input("Indique su primer apellido: ")
+apellido2 = raw_input("Indique su segundo apellido: ")
+if nombre == "Listillo" :
+    print "Que pasa ...."
+    print "...pardillo..."
+else:
+   print "Bienvenido Sr ” , apellido1 , " " , apellido2 , "," , nombre
