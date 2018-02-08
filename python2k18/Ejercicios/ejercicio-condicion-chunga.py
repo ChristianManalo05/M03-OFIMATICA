3a772617316311804bdb526e1be284d980a0abcc
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-08 12:04:19 +0100

    Create ejercicio-condicion-chunga.py

---
+++ python2k18/Ejercicios/ejercicio-condicion-chunga.py
@@ -0,0 +1,12 @@
+# coding: utf8
+# Christian Mañibo
+# 8/2/2018
+
+
+numero=input ("Introduce número : ")
+
+if (numero%2==0) or (numero>=-10 and numero<=40) or (numero<0):
+    print "Se cumple condición chunga"
+
+else:
+    print "No cumple condición chunga"
