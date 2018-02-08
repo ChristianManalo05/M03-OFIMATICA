99b62efb4f8eaccb735be5dfadb1398e51659bf3
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-08 12:03:40 +0100

    Create ejercicio-pares.py

---
+++ python2k18/Ejercicios/ejercicio-pares.py
@@ -0,0 +1,13 @@
+#coding: utf8
+# Christian Manalo Mañibo
+# 8/2/2018
+
+numero=input ("Introduce numero : ")
+
+if numero % 2 == 0 : 
+    print "Qué bonito número par"
+
+else:
+    print "Qué número mas vulgar"
+
+#FIN del código
