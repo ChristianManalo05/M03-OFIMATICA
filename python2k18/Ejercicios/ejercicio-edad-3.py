165e6f81b1acc20e2fa4f7d3942f5acfd74f077c
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-08 10:19:45 +0100

    Create ejercicio-edad-3.py

---
+++ python2k18/Ejercicios/ejercicio-edad-3.py
@@ -0,0 +1,10 @@
+# coding: utf8
+# Christian Manalo Mañibo
+# 8/2/2018
+
+edad=input("Introduce tu edad: ")
+
+if (edad>=18 and edad<=23) or edad==17 : 
+    print "Puede entrar en la sesión de jovenes"
+
+#FIN del código
