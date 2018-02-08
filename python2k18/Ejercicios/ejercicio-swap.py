71400e2c62f7ae9f7ea313d4c20b1fa9a7cb93b2
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-08 17:54:34 +0100

    Create ejercicio-swap.py

---
+++ python2k18/Ejercicios/ejercicio-swap.py
@@ -0,0 +1,36 @@
+# coding: utf8
+# Christian Manalo Mañibo
+# 08/02/2018
+
+mano_der="movil"
+mano_izq="bocadillo"
+
+print "----------------------Variable-----------------------"
+
+
+
+print "mano_derecha -->" , mano_der
+
+print "|"
+
+print "mano_izquierda -->" , mano_izq
+
+
+# Cambiem els variables
+
+mano_temp = mano_der
+mano_der = mano_izq
+mano_izq = mano_temp
+
+print " --------------Resultat del canvi------------------"
+
+print "|"
+
+print "mano_derecha -->" , mano_der
+
+print "|"
+
+print "mano_izquierda -->" , mano_izq
+
+
+#FIN DEL CODIGO
