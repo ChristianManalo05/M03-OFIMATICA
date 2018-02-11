12f1e0a04c6722c34c937d91c574bfee24d0ce27
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-11 19:29:47 +0100

    Update ejercicio-mayor-menor.py

--- python2k18/Ejercicios/ejercicio-mayor-menor.py
+++ python2k18/Ejercicios/ejercicio-mayor-menor.py
@@ -8,7 +8,25 @@ C=input("Introduce valor C: ")
 
 if (A==B) and (A==C) :
     print "Hay 3 valores iguales"
-
 else:
     if  ((A==B) and (A!=C)) or ((B==C) and (B!=A)) or ((A==C) and (C!=B)):
         print "Hay 2 valores iguales"
+    else:
+        if (A>B) and (A>C) :
+            if B<C :
+                print "Valor A es mayor y Valor B es menor"
+            else:
+                print "Valor A es mayor y Valor C es menor"
+        else:
+            if (B>A) and (B>C) :
+                if A<C :
+                    print "Valor B es mayor y Valor A es menor"
+                else:
+                    print "Valor B es mayor y Valor C es menor"
+            else:
+                if (C>A) and (C>B) :
+                    if A<B:
+                        print "Valor C es mayor y Valor A es menor"
+                    else:
+                        print "Valor C es mayor y Valor B es menor"
+        
