75d511a8ef25644795617921feb957fba0b83290
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-12 18:58:17 +0100

    Create if...elif...else...-1.py

---
+++ python2k18/Ejercicios/if...elif...else...-1.py
@@ -0,0 +1,19 @@
+# coding:utf8   
+# Christian Manalo Mañibo
+# 12/02/2018
+
+dividiendo=input("Escriba el dividiendo: ")
+
+divisor=input("Escriba el divisor: ")
+if divisor == 0:
+    print "No se puede dividir entre 0. Prueba de nuevo." 
+    divisor=input("Escriba el divisor: ")
+
+cociente= dividiendo // divisor
+resto= dividiendo % divisor 
+
+
+if dividiendo % divisor == 0:
+    print "La división es exacta. Cociente: " ,  cociente
+else:
+    print "La división no es exacta. Cociente: " , cociente, "Resto: ", resto 
