5c19be908c311b000ac83a86801cef3baf14f8f4
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-02-21 17:06:07 +0100

    Create ejercicio-mayor-elif.py

---
+++ python2k18/Ejercicios/ejercicio-mayor-elif.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# Christian Manalo Mañibo
+# 8/2/2018
+
+print "Que número es mas grande?" 
+
+# Introduce el usuario el numero 1
+num1 = input("Introduce el primer número: ")
+
+# Introduce el usuario el numero  2
+num2 = input("Introduce el segundo número: ")
+
+# Si el número 1 es mas grande que numero 2
+if (num1 > num2):
+    print "Número 1 es mas grande que Número 2"
+
+# Si no lo es
+elif (num1 < num2):
+        print "Número 2 es mas grande que Número 1  "
+
+    # Si son iguales
+    else:
+        print "Número 1 y Número 2 son iguales "
+        
+#FIN del código
