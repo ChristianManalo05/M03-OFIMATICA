d2aed04bba2b11b025f084a0ea2cebefe8fa7048
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-03-09 08:30:28 +0100

    Create ejercicio-bucle-contar2.py

---
+++ python2k18/Ejercicios/ejercicio-bucle-contar2.py
@@ -0,0 +1,39 @@
+# coding: utf8
+# Christian Mañibo
+# 09/05/2018
+
+
+import os
+os.system("clear")
+
+print"""
+
+-----BUCLE CONTAR-----
+
+"""
+
+# Inicializaciones
+salir = "N"
+num=1
+limit=input("Hasta cuanto quieres contar?: ")
+
+if (num<limit) :
+    while ( salir=="N" ):
+        # Hago cosas
+        print num
+    
+        # Incremento
+        num = num + 1
+        # Activo indicador de salida si toca
+        if ( num>limit ): # Condición de salida
+            salir = "S"
+else :
+    while ( salir=="N" ):
+        # Hago cosas
+        print num
+    
+        # Incremento
+        num = num - 1
+        # Activo indicador de salida si toca
+        if ( num<limit ): # Condición de salida
+            salir = "S"
