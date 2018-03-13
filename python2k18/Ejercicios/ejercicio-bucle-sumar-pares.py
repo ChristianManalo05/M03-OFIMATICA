f7acc59022f9499e5475f66e6aba15cfe88121ed
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-03-13 13:21:15 +0100

    Create ejercicio-bucle-sumar-pares.py

---
+++ python2k18/Ejercicios/ejercicio-bucle-sumar-pares.py
@@ -0,0 +1,39 @@
+# coding: utf8
+# Christian Mañibo
+# 13/03/2018
+# Corrección/Mejora
+
+import os
+os.system("clear")
+
+print "###### BUCLE SUMAR ######"
+
+# Inicializaciones
+salir = "N"
+num = 1
+maximo=5
+resultado = 0
+
+
+while ( salir=="N" ):
+    # Hago cosas
+    
+    if (num%2==0): # Es par
+
+       print num,
+       
+       if (num<= maximo-2):
+
+           print "+" ,
+        
+       resultado = resultado + num
+            
+    # Incremento
+    
+    num = num + 1
+    
+    # Activo indicador de salida si toca
+    if ( num > maximo): # Condición de salida
+        salir = "S"
+
+print "=", resultado
