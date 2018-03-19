81ffbb2000f680e6d7e84630b108e8fee4a7cd5a
Christian Manalo <36135156+ChristianManalo05@users.noreply.github.com>
2018-03-19 23:03:33 +0100

    Create ejercicio-bucle-vuelta_derecha_turnos.py

---
+++ python2k18/Ejercicios/ejercicio-bucle-vuelta_derecha_turnos.py
@@ -0,0 +1,34 @@
+#coding: utf8
+# Christian Manalo Mañibo
+# 15/03/2018
+
+
+import os
+os.system ("clear")
+
+print "-EJERCICIO BUCLE : VUELTA DERECHA TURNOS -"
+# Variables
+salir="N"
+vuelta=1
+turnos=input("Introduce cuantos turnos desea: ")
+
+while ( salir=="N" ):
+	
+	# Hago cosas
+	if vuelta%8==1 or vuelta%8==2 :
+		print vuelta ,"-> arriba"
+	elif vuelta%8==3 or vuelta%8==4 :
+		print vuelta , "-> derecha"
+	elif vuelta%8==5 or vuelta%8==6 :
+		print vuelta , "-> abajo"
+	elif vuelta%8==7 or vuelta%8==0 :
+		print vuelta ,"-> izquierda"
+
+	# Incremento
+	vuelta = vuelta + 1 
+
+	# Activo indicador de salida si toca
+	if (vuelta>turnos*8): # Condición de salida
+		
+		salir = "S"
+    print "FIN DE LAS VUELTAS"
